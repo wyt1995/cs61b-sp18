9f56166eb99ae6cf727ada1823956e616bd678a6
Yutong Wang <aeo9539@gmail.com>
2024-03-15 00:19:10 -0400

    Project 1b

--- lab3/Flik/FlikTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-
-public class FlikTest {
-    @Test
-    public void testIsSameNumber(){
-        assertTrue(Flik.isSameNumber(1024, 1024));
-    }
-}
