33ebbd4a7ebcd7e3d27a6ab2245cba69e2e2ebc3
Yutong Wang <aeo9539@gmail.com>
2024-03-14 22:33:08 -0400

    finished bfs

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +13,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int source;
+    private int target;
+    private Maze maze;
+    private boolean targetFound = false;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        this.maze = m;
+        this.source = maze.xyTo1D(sourceX, sourceY);
+        this.target = maze.xyTo1D(targetX, targetY);
+        this.distTo[source] = 0;
+        this.edgeTo[source] = source;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> fringe = new ArrayDeque<>();
+        fringe.add(source);
+        marked[source] = true;
+        announce();
+        while (!fringe.isEmpty()) {
+            int relaxed = fringe.remove();
+            for (int v : maze.adj(relaxed)) {
+                // stop iteration as long as target is found
+                if (v == target) {
+                    targetFound = true;
+                    return;
+                }
+
+                // update distTo, edgeTo, and marked for every unvisited vertex
+                if (!marked[v]) {
+                    edgeTo[v] = relaxed;
+                    distTo[v] = distTo[relaxed] + 1;
+                    announce();
+                    marked[v] = true;
+                    fringe.add(v);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
