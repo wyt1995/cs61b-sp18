73b4f5cf2dfb3f4aea02365cc5d5909e2582edd1
Yutong Wang <aeo9539@gmail.com>
2024-03-14 23:04:20 -0400

    cycle detection

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,43 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int start;
+    private Maze maze;
+    private boolean hasCycle = false;
 
     public MazeCycles(Maze m) {
         super(m);
+        this.maze = m;
+        this.start = maze.xyTo1D(1, 1);
+        distTo[start] = 0;
+        edgeTo[start] = start;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsCycle(start);
     }
 
-    // Helper methods go here
+    private void dfsCycle(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int u : maze.adj(v)) {
+            if (marked[u] && !isParent(u, v)) {
+                hasCycle = true;
+                return;
+            }
+            if (!marked[u]) {
+                edgeTo[u] = v;
+                announce();
+                distTo[u] = distTo[v] + 1;
+                dfsCycle(u);
+            }
+        }
+    }
+
+    private boolean isParent(int visited, int curr) {
+        return distTo[visited] == distTo[curr] - 1;
+    }
 }
 
