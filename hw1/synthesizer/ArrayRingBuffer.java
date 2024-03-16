cbeb2df4830acc04a624ef424446e13b222d958d
Yutong Wang <aeo9539@gmail.com>
2024-03-15 20:09:59 -0400

    hw1: guitar string

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,81 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // Create new array with capacity elements.
+        // first, last, and fillCount should all be set to 0.
+        // this.capacity should be set appropriately. Note that the local variable
+        // here shadows the field we inherit from AbstractBoundedQueue, so
+        // you'll need to use this.capacity to set the capacity.
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        // Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Buffer is full");
+        }
+        rb[last] = x;
+        fillCount += 1;
+        last = maintainIndex(last);
+    }
+
+    private int maintainIndex(int index) {
+        index += 1;
+        if (index == capacity) {
+            index = 0;
+        }
+        return index;
+    }
+
+    /**
+     * Dequeue the oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        // Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) {
+            throw new RuntimeException("Nothing in the buffer");
+        }
+        T item = rb[first];
+        rb[first] = null;
+        fillCount -= 1;
+        first = maintainIndex(first);
+        return item;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        // Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Nothing in the buffer");
+        }
+        return rb[first];
+    }
+
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,22 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        for (int i = 0; i < 10; i++) {
+            arb.enqueue(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            assertEquals(i, (int) arb.dequeue());
+        }
+        for (int i = 0; i < 5; i++) {
+            arb.enqueue(10 * i);
+        }
+        for (int i = 0; i < 5; i++) {
+            assertEquals(i + 5, (int) arb.dequeue());
+        }
+        for (int i = 0; i < 5; i++) {
+            assertEquals(10 * i, (int) arb.dequeue());
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
