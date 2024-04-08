bf38981b20ef3f7c8b1fdcf64bd665544c3b601f
Yutong Wang <aeo9539@gmail.com>
2024-04-07 20:25:26 -0400

    hw3: hash code

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -10,26 +10,31 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
+        if (this == o) return true;
+        if (o instanceof SimpleOomage) {
+            SimpleOomage other = (SimpleOomage) o;
+            return this.red == other.red && this.green == other.green && this.blue == other.blue;
+        }
         return false;
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            int prime = 61;
+            int result = 1;
+            result += red / 5 * prime * prime;
+            result += green / 5 * prime;
+            result += blue / 5;
+            return result;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class TestComplexOomage {
 
     @Test
@@ -33,18 +35,27 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
-     * that shows the flaw in the hashCode function.
-     */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
-        // Your code here.
+        for (int i = 0; i < 100; i += 1) {
+            List<Integer> color = randomList();
+            color.add(1);
+            color.add(2);
+            deadlyList.add(new ComplexOomage(color));
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
+
+    private List<Integer> randomList() {
+        List<Integer> random = new ArrayList<>();
+        for (int i = 0; i < 5; i += 1) {
+            random.add(StdRandom.uniform(200, 256));
+        }
+        return random;
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
